0a0e1908dc8565812c76988e0645c5755dd06ffa
Sohil Shah <sohil@sohil.local>
2025-04-16 19:52:17 +0530

    Use hardcoded port and remove conflicting startCommand

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-# Use default port 8000 if PORT is not set
-PORT=${PORT:-8000}
-exec uvicorn main:app --host 0.0.0.0 --port $PORT 
+# Use a hardcoded port 8000
+exec uvicorn main:app --host 0.0.0.0 --port 8000 
